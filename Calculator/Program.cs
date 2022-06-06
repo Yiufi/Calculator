5409b8c78e557c438249719cbe05524e6c2a4206
Diego <105529723+Yiufi@users.noreply.github.com>
2022-06-06 10:01:51 -0300

    Division

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -8,6 +8,7 @@ namespace Calculator
     {
       Soma();
       Subtracao();
+      Divisao();
     }
 
     static void Soma()
@@ -42,6 +43,23 @@ namespace Calculator
 
       float resultado = v1 - v2;
       Console.Write($"O resultado da Subtração é {resultado}");
+      Console.ReadKey();
+    }
+
+    static void Divisao()
+    {
+      Console.Clear();
+
+      Console.Write("Primeiro valor: ");
+      float v1 = float.Parse(Console.ReadLine());
+
+      Console.Write("Segundo valor: ");
+      float v2 = float.Parse(Console.ReadLine());
+
+      Console.WriteLine("");
+
+      float resultado = v1 - v2;
+      Console.Write($"O resultado da Divisão é {resultado}");
       Console.ReadKey();
     }
   }
